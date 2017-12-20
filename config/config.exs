1162fa1fb7b7591ad98693ee3b505ef3ddb3e93c
Mylan Connolly <mconnolly@iotrak.com>
2017-12-20 12:26:59 -0500

    Added tests and fixed config

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-if File.exists?("./config.secret.exs") do
+if File.exists?(Path.join([File.cwd!(), "config", "config.secret.exs"])) do
   import_config "config.secret.exs"
 end
--- test/cumulus_test.exs
+++ test/cumulus_test.exs
@@ -1,8 +1,23 @@
 defmodule CumulusTest do
   use ExUnit.Case
+
   doctest Cumulus
 
-  test "greets the world" do
-    assert Cumulus.hello() == :world
+  describe "Cumulus.object_url/2" do
+    test "it returns a properly-formatted Google Cloud Storage URL" do
+      assert Cumulus.object_url("123", "456") == "https://www.googleapis.com/storage/v1/b/123/o/456"
+    end
+  end
+
+  describe "Cumulus.bucket_upload_url/1" do
+    test "it returns a properly-formatted Google Cloud Storage URL" do
+      assert Cumulus.bucket_upload_url("123") == "https://www.googleapis.com/upload/storage/v1/b/123/o"
+    end
+  end
+
+  describe "Cumulus.bucket_url/1" do
+    test "it returns a properly-formatted Google Cloud Storage URL" do
+      assert Cumulus.bucket_url("123") == "https://www.googleapis.com/storage/v1/b/123"
+    end
   end
 end
