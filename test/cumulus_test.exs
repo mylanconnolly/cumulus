4c31b73febcd7c1d50349c3f4d4089001a20c593
Mylan Connolly <mconnolly@iotrak.com>
2017-12-20 12:27:56 -0500

    Cleaned up tests slightly

--- test/cumulus_test.exs
+++ test/cumulus_test.exs
@@ -1,23 +1,25 @@
 defmodule CumulusTest do
   use ExUnit.Case
 
+  import Cumulus
+
   doctest Cumulus
 
   describe "Cumulus.object_url/2" do
     test "it returns a properly-formatted Google Cloud Storage URL" do
-      assert Cumulus.object_url("123", "456") == "https://www.googleapis.com/storage/v1/b/123/o/456"
+      assert object_url("123", "456") == "https://www.googleapis.com/storage/v1/b/123/o/456"
     end
   end
 
   describe "Cumulus.bucket_upload_url/1" do
     test "it returns a properly-formatted Google Cloud Storage URL" do
-      assert Cumulus.bucket_upload_url("123") == "https://www.googleapis.com/upload/storage/v1/b/123/o"
+      assert bucket_upload_url("123") == "https://www.googleapis.com/upload/storage/v1/b/123/o"
     end
   end
 
   describe "Cumulus.bucket_url/1" do
     test "it returns a properly-formatted Google Cloud Storage URL" do
-      assert Cumulus.bucket_url("123") == "https://www.googleapis.com/storage/v1/b/123"
+      assert bucket_url("123") == "https://www.googleapis.com/storage/v1/b/123"
     end
   end
 end
